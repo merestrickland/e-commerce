c20849aa389ad21ff9d4817a3895b9f8bec45e56
merestrickland <mere.strickland@gmail.com>
2019-08-26 16:58:53 -0400

    sign in component and custom button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.components'
 import ShopPage from './pages/shop/shop.component'
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component'
 import Header from './components/header/header.component'
 
 const HatsPage = () => (
@@ -18,6 +19,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUp} />
+
       </Switch>
     </div>
   );
